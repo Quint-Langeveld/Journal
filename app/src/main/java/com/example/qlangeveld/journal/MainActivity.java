1ae09f2ea9810346663c2d6d025816c1735f3951
Quint-Langeveld <quintlangeveld@hotmail.com>
2018-11-27 22:51:20 +0100

    progression commit

--- app/src/main/java/com/example/qlangeveld/journal/MainActivity.java
+++ app/src/main/java/com/example/qlangeveld/journal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.qlangeveld.journal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toInputActivity() {
+        Intent intent = new Intent(this, InputActivity.class);
+        startActivity(intent);
     }
 
 }
